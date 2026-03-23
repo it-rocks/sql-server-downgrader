309a96b5e0cea38fb4c9b04d3bbb79378e60be98
it-rocks <info@it-rocks.be>
2026-03-23 06:07:43 +0100

    Add DatabaseInstance model with properties for instance name, version, status, and connection details

---
+++ Models/DatabaseInstance.cs
@@ -0,0 +1,7 @@
+public class DatabaseInstance
+{
+    public string InstanceName { get; set; }
+    public string Version { get; set; }
+    public string Status { get; set; }
+    public string ConnectionDetails { get; set; }
+}
